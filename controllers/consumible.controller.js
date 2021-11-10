2edb4375f517fdc07c778249019c9b22dceac140
Mauricioagreda <mauricio.agreda.d2@gmail.com>
2021-11-10 09:52:42 -0300

    Cambios Controllers

--- controllers/consumible.controller.js
+++ controllers/consumible.controller.js
@@ -2,31 +2,31 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../auth')
 
-router.get('/consumible', auth.isLoggedIn, (req, res) => {
-    res.render('./consumible/index', { 
-        title: "Consumible",
+router.get('/consumibles', auth.isLoggedIn, (req, res) => {
+    res.render('./consumibles/index', { 
+        title: "Consumibles",
         user: req.user
     })
 })
 
-router.get('/consumible/:id/edit', auth.isLoggedIn, (req, res) => {
-    res.render('./consumible/edit', { 
+router.get('/consumibles/:id/edit', auth.isLoggedIn, (req, res) => {
+    res.render('./consumibles/edit', {
         title: "Consumible",
-        roleId: req.params.id,
+        consumibleId: req.params.id,
         user: req.user
     })
 })
 
-router.get('/consumible/:id/delete', auth.isLoggedIn, (req, res) => {
-    res.render('./consumible/delete', { 
+router.get('/consumibles/:id/delete', auth.isLoggedIn, (req, res) => {
+    res.render('./consumibles/delete', { 
         title: "Consumible",
-        roleId: req.params.id,
+        consumibleId: req.params.id,
         user: req.user 
     })
 })
 
-router.get('/consumible/add', auth.isLoggedIn, (req, res) => {
-    res.render('./consumible/add', { 
+router.get('/consumibles/add', auth.isLoggedIn, (req, res) => {
+    res.render('./consumibles/add', { 
         title: "Consumible",
         user: req.user       
     })
