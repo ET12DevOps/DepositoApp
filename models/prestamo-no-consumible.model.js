60e4c7bcf3a740ef5af33501d9208da50eb2ffee
Jonathan Velazquez <jonathanvgms@gmail.com>
2021-12-04 20:39:11 -0300

    Fix model

--- models/prestamo-no-consumible.model.js
+++ models/prestamo-no-consumible.model.js
@@ -10,9 +10,13 @@ module.exports = (sequelize, DataTypes) => {
     },
     idNoConsumible: {
       type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
     },
     nroPrestamo: {
       type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
     },
     devuelto: {
       type: DataTypes.BOOLEAN
