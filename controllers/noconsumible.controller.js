6c5cde4d008c94ed0b85ab010ef6a45cab9b2f06
LThames22 <likrein22@gmail.com>
2021-12-13 15:44:35 -0300

    13/12 15:44

--- controllers/noconsumible.controller.js
+++ controllers/noconsumible.controller.js
@@ -3,30 +3,30 @@ const router = express.Router()
 const auth = require('../auth')
 
 router.get('/noconsumibles', auth.isLoggedIn, (req, res) => {
-    res.render('./noconsumibles/index', { 
+    res.render('./noconsumible/index', { 
         title: "No consumibles",
-        user: req.user
+        user: req.user  
     })
 })
 
-router.get('/noconsumibles/:id/edit', auth.isLoggedIn, (req, res) => {
-    res.render('./noconsumibles/edit', {
+router.get('/noconsumible/:id/edit', auth.isLoggedIn, (req, res) => {
+    res.render('./noconsumible/edit', {
         title: "No consumibles",
         noconsumibleId: req.params.id,
         user: req.user
     })
 })
 
-router.get('/noconsumibles/:id/delete', auth.isLoggedIn, (req, res) => {
-    res.render('./noconsumibles/delete', { 
+router.get('/noconsumible/:id/delete', auth.isLoggedIn, (req, res) => {
+    res.render('./noconsumible/delete', { 
         title: "No consumibles",
         noconsumibleId: req.params.id,
         user: req.user 
     })
 })
 
-router.get('/noconsumibles/add', auth.isLoggedIn, (req, res) => {
-    res.render('./noconsumibles/add', { 
+router.get('/noconsumible/add', auth.isLoggedIn, (req, res) => {
+    res.render('./noconsumible/add', { 
         title: "No consumibles",
         user: req.user       
     })
